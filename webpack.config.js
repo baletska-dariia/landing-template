0b6441298cedb9518cdf181e611b400cec43ad70
Dariya Baletskaya <baletskaya.dariya@gmail.com>
2019-09-29 15:01:38 +0300

    fix about us responsive styles

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,26 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'sass-loader']
-            })
-        }]
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ]
+            },
+        ]
     },
     plugins: [
         new ExtractTextPlugin('style.css')
